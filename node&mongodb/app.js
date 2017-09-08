b3749e15fea67947634785cb7f4895ba25cff090
wayne <1372887803@qq.com>
2017-09-08 21:40:09 +0800

    5

--- node&mongodb/app.js
+++ node&mongodb/app.js
@@ -1,10 +1,15 @@
 
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.set('views', './views');
+app.set('views', './views/pages');
 app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.listen(port);
 
 console.log('movie started on port ' + port);
