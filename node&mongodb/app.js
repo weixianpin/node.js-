fa606da4b3204b804012f09485fb0ed90b9cfec3
wayne <1372887803@qq.com>
2017-09-08 19:50:34 +0800

    4

---
+++ node&mongodb/app.js
@@ -0,0 +1,35 @@
+
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+app.listen(port);
+
+console.log('movie started on port ' + port);
+
+//index page
+app.get('/', function (req, res) {
+	res.render('index', {
+		title: 'movie 首页'
+	});
+});
+//list page
+app.get('/admin/list', function (req, res) {
+	res.render('list', {
+		title: 'movie 列表页'
+	});
+});
+//detail page
+app.get('/movie/:id', function (req, res) {
+	res.render('detail', {
+		title: 'movie 详细页'
+	});
+});
+//admin page
+app.get('/admin/movie', function (req, res) {
+	res.render('admin', {
+		title: 'movie 登录页'
+	});
+});
