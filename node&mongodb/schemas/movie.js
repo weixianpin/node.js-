3dc46d68ca0ae080491331c188f3d1762bba60e7
wayne <1372887803@qq.com>
2017-09-10 20:02:53 +0800

    schemas-movie.js

---
+++ node&mongodb/schemas/movie.js
@@ -0,0 +1,50 @@
+
+var mongoose = require('mongoose');
+
+var MovieSchema = new mongoose.Schema({
+	director: String,
+	title: String,
+	language: String,
+	country: String,
+	summary: String,
+	flash: String,
+	poster: String,
+	year: Number,
+	meta: {
+		createAt:{
+			type: Date,
+			default: Date.now()
+		},
+		updateAt:{
+			type: Date,
+			default: Date.now()
+		}
+	}
+});
+
+//每次存储数据前，调用该方法
+MovieSchema.pre('save', function (next) {
+	if (this.isNew) {
+		//新加数据 创建时间和更新时间一样
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	}else {
+		this.meta.updateAt = Date.now();
+	}
+	next();
+});
+
+MovieSchema.statics = {
+	//取到数据库所有数据
+	fetch: function(cb) {
+		return this
+		.find({})//查询数据
+		.sort('meta.updateAt')
+		exec(cb);
+	}, 
+	findById: function(id, cb) {
+		return this
+		.findOne({_id: id})//查询数据
+		exec(cb);
+	}
+};
+module.exports = MovieSchema;
