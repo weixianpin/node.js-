581cc9310876194236109dcf1c11c6c2e1a53e27
wayne <1372887803@qq.com>
2017-09-10 16:41:52 +0800

    update Sunday

--- node&mongodb/app.js
+++ node&mongodb/app.js
@@ -8,33 +8,87 @@ var app = express();
 
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.listen(port);
-
 console.log('movie started on port ' + port);
 
 //index page
 app.get('/', function (req, res) {
 	res.render('index', {
-		title: 'movie 首页'
+		title: 'movie 首页',
+		movies: [{
+			title: '机械战警',
+			_id: 1,
+			poster: 'http://img31.mtime.cn/pi/2013/12/13/112514.36500695_1000X1000.jpg'
+		}, {
+			title: '机械战警',
+			_id: 2,
+			poster: 'http://img31.mtime.cn/pi/2013/12/13/112514.36500695_1000X1000.jpg'
+		}, {
+			title: '机械战警',
+			_id: 3,
+			poster: 'http://img31.mtime.cn/pi/2013/12/13/112514.36500695_1000X1000.jpg'
+		}, {
+			title: '机械战警',
+			_id: 4,
+			poster: 'http://img31.mtime.cn/pi/2013/12/13/112514.36500695_1000X1000.jpg'
+		}, {
+			title: '机械战警',
+			_id: 5,
+			poster: 'http://img31.mtime.cn/pi/2013/12/13/112514.36500695_1000X1000.jpg'
+		}, {
+			title: '机械战警',
+			_id: 6,
+			poster: 'http://img31.mtime.cn/pi/2013/12/13/112514.36500695_1000X1000.jpg'
+		}]
 	});
 });
 //list page
 app.get('/admin/list', function (req, res) {
 	res.render('list', {
-		title: 'movie 列表页'
+		title: 'movie 列表页',
+		movies: [{
+			title: '机械战警',
+			_id: 1,
+			director: '何塞·帕迪里亚',
+			country: '美国',
+			year: '2014',
+			language: '英语',
+			flash: 'http://player.youku.com/player.php/sid/XNjMyMTkzODcy/v.swf'
+		}]
 	});
 });
 //detail page
 app.get('/movie/:id', function (req, res) {
 	res.render('detail', {
-		title: 'movie 详细页'
+		title: 'movie 详细页',
+		movie: {
+			director: '何塞·帕迪里亚',
+			country: '美国',
+			title: '机械战警',
+			year: '2014',
+			poster: 'http://img31.mtime.cn/pi/2013/12/13/112514.36500695_1000X1000.jpg',
+			language: '英语',
+			flash: 'http://player.youku.com/player.php/sid/XNjMyMTkzODcy/v.swf',
+			summary: '2028年，专事军火开发的机器人公司Omni Corp生产了大量装备精良的机械战警，他们被投入到维和和惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什饰）和儿子大卫却再难从他身上感觉亲人的温暖。感知到妻儿的痛苦，墨菲决心向策划杀害自己的犯罪头子展开反击。'
+
+		}
 	});
 });
 //admin page
 app.get('/admin/movie', function (req, res) {
 	res.render('admin', {
-		title: 'movie 登录页'
+		title: 'movie 后台录入页',
+		movie: {
+			title: '',
+			director: '',
+			country: '',
+			year: '',
+			poster: '',
+			flash: '',
+			summary: '',
+			language: ''
+		}
 	});
 });
