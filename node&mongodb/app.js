aa94a8905bfdc0769b0a18221d74375301423765
wayne <1372887803@qq.com>
2017-09-21 14:54:04 +0800

    updata app.js

--- node&mongodb/app.js
+++ node&mongodb/app.js
@@ -9,7 +9,7 @@ var Movie = require('./modules/movie.js');
 var port = process.env.PORT || 3000;
 var app = express();
 
-mongoose.connect('mongoose.db: //localhost/movie');//链接数据库
+mongoose.connect('mongodb://localhost/movie');//链接数据库
 
 app.locals.moment = require('moment');
 app.set('views', './views/pages');
@@ -31,6 +31,89 @@ app.get('/', function (req, res) {
 		}); 
 	});
 });
+
+
+//detail page
+app.get('/movie/:id', function (req, res) {
+	var id = req.params.id;
+	Movie.findById(id, function(err, movie){
+	res.render('detail', {
+			title: 'movie' + movie.title,
+			movie: movie
+		});
+	});
+	
+});
+
+//admin page
+app.get('/admin/movie', function (req, res) {
+	res.render('admin', {
+		title: 'movie 后台录入页',
+		movie: {
+			title: '',
+			director: '',
+			country: '',
+			year: '',
+			poster: '',
+			flash: '',
+			summary: '',
+			language: ''
+		}
+	});
+});
+
+//admin update movie
+app.get('/admin/update/:id', function(req, res) {
+	var id = req.params.id;
+	if(id) {
+		Movie.findById(id, function(err, movie) {
+			res.render('admin', {
+				title: 'movie 后台更新页面',
+				movie: movie
+			});
+		});
+	}
+});
+
+//admin post movie
+app.post('/admin/movie/new', function(req, res) {
+	var id = req.body.movie._id;
+	var movieObj = req.body.movie;//post 过来的movie
+	var _movie;
+
+	if(id !== 'undefined') {
+		Movie.findById(id, function(err, movie) {
+			if(err) {
+				console.log(err);
+			}
+			_movie = _.extend(movie, movieObj);//将post过来的数据替换原先的数据
+			_movie.save(function(err, movie) {
+				if(err) {
+					console.log(err);
+				}
+				res.redirect('/movie/' + movie._id);
+			});
+		});
+	}else {
+		_movie = new Movie({
+			title: movieObj.title,
+			director: movieObj.director,
+			country: movieObj.country,
+			year: movieObj.year,
+			language: movieObj.language,
+			poster: movieObj.poster,
+			flash: movieObj.flash,
+			summary: movieObj.summary
+		});
+		_movie.save(function(err, movie) {
+				if(err) {
+					console.log(err);
+				}
+				res.redirect('/movie/' + movie._id);
+		});
+	}
+});
+
 //list page
 app.get('/admin/list', function (req, res) {
 	Movie.fetch(function(err, movies) {
@@ -58,85 +141,4 @@ app.delete('/admin/list', function(req, res) {
 			}
 		});
 	}
-});
-
-//detail page
-app.get('/movie/:id', function (req, res) {
-	var id = req.params.id;
-	Movie.findById(id, function(err, movie){
-	res.render('detail', {
-			title: 'movie' + movie.title,
-			movie: movie
-		});
-	});
-	
-});
-
-//admin update movie
-app.get('/admin/update/:id', function(req, res) {
-	var id = req.params.id;
-	if(id) {
-		Movie.findById(id, function(err, movie) {
-			res.render('admin', {
-				title: 'movie 后台更新页面',
-				movie: movie
-			});
-		});
-	}
-});
- 
-//admin post movie
-app.post('/admin/movie/new', function(req, res) {
-	var id = req.body.movie._id;
-	var movieObj = req.body.movie;//post 过来的movie
-	var _movie;
-
-	if(id !== 'undefined') {
-		Movie.findById(id, function(err, movie) {
-			if(err) {
-				console.log(err);
-			}
-			_movie = _.extend(movie, movieObj);//将post过来的数据替换原先的数据
-			_movie.save(function(err, movie) {
-				if(err) {
-					console.log(err);
-				}
-				res.redirect('/movie/' + movie.id);
-			});
-		});
-	}else {
-		_movie = new Movie({
-			title: movieObj.title,
-			director: movieObj.director,
-			country: movieObj.country,
-			year: movieObj.year,
-			language: movieObj.language,
-			poster: movieObj.poster,
-			flash: movieObj.flash,
-			summary: movieObj.summary
-		});
-		_movie.save(function(err, movie) {
-				if(err) {
-					console.log(err);
-				}
-				res.redirect('/movie/' + movie.id);
-			});
-	}
-});
-
-//admin page
-app.get('/admin/movie', function (req, res) {
-	res.render('admin', {
-		title: 'movie 后台录入页',
-		movie: {
-			title: '',
-			director: '',
-			country: '',
-			year: '',
-			poster: '',
-			flash: '',
-			summary: '',
-			language: ''
-		}
-	});
 });
