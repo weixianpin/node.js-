6cb9215771b69b56fda2baa274fb505cef8899de
wayne <1372887803@qq.com>
2017-09-26 21:46:32 +0800

    3

--- node&mongodb/app.js
+++ node&mongodb/app.js
@@ -8,11 +8,9 @@ var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 var port = process.env.PORT || 3000;
 var app = express();
-var routes = require('./config/routes.js')(app);
 
 mongoose.connect('mongodb://localhost/movie');//链接数据库
 
-app.locals.moment = require('moment');
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
@@ -25,8 +23,17 @@ app.use(session({
 		collection: 'sessions'
 	})
 }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+if('development' === app.get('env')) {
+	app.set('showStackError', true);
+	app.user(express.logger(':method :url :status'));
+	app.locals.pretty = true;
+	mongoose.set('debug', true);
+}
+var routes = require('./config/routes.js')(app);
 app.listen(port);
+app.locals.moment = require('moment');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 console.log('movie started on port' + port);
-
-
