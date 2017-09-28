8215c51f335d5d0713e2a441d2d78fbddca26b05
wayne <1372887803@qq.com>
2017-09-28 19:43:34 +0800

    comments.js

---
+++ node&mongodb/app/schemas/comments.js
@@ -0,0 +1,43 @@
+
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.Schema({
+	
+	meta: {
+		createAt:{
+			type: Date,
+			default: Date.now()
+		},
+		updateAt:{
+			type: Date,
+			default: Date.now()
+		}
+	}
+});
+
+//每次存储数据前，调用该方法
+CommentSchema.pre('save', function (next) {
+	if (this.isNew) {
+		//新加数据 创建时间和更新时间一样
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	}else {
+		this.meta.updateAt = Date.now();
+	}
+	next();
+});
+
+CommentSchema.statics = {
+	//取到数据库所有数据
+	fetch: function(cb) {
+		return this
+		.find({})//查询数据
+		.sort('meta.updateAt')
+		.exec(cb);
+	}, 
+	findById: function(id, cb) {
+		return this
+		.findOne({_id: id})//查询数据
+		.exec(cb);
+	}
+};
+module.exports = CommentSchema;
