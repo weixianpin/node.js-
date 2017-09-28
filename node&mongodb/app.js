3e6357c5982bdb5c71882f5b8ab9676a9d700f28
wayne <1372887803@qq.com>
2017-09-28 15:06:41 +0800

    app folder

--- node&mongodb/app.js
+++ node&mongodb/app.js
@@ -23,3 +23,16 @@ app.use(session({
 	})
 }));
 
+if('development' === app.get('env')) {
+	app.set('showStackError', true);
+	app.use(express.logger(':method :url :status'));
+	app.locals.pretty = true;
+	mongoose.set('debug', true);
+}
+var routes = require('./config/routes.js')(app);
+app.listen(port);
+app.locals.moment = require('moment');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+console.log('movie started on port' + port);
