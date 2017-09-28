26f02fc4d1289b48a79b0a6ca7c33edccb5a29c0
wayne <1372887803@qq.com>
2017-09-28 15:08:55 +0800

    controllers/index.js

---
+++ node&mongodb/app/controllers/index.js
@@ -0,0 +1,15 @@
+//index page
+exports.index = function (req, res) {
+	console.log('user in session: ');
+	console.log(req.session.user);
+
+	Movie.fetch(function(err, movies) {
+		if (err) {
+				console.log(err);
+		}
+		res.render('index', {
+			title: 'movie 首页',
+			movies: movies
+		}); 
+	});
+};
