ea92bfc41cdf8f8b530ad671eeb03bce83962262
wayne <1372887803@qq.com>
2017-10-08 17:58:54 +0800

    2

--- node&mongodb/app/controllers/movie.js
+++ node&mongodb/app/controllers/movie.js
@@ -2,6 +2,22 @@ var _ = require('underscore');
 var Movie = require('../modules/movie.js');
 var Comment = require('../modules/comment.js');
 //detail page
+exports.detail = function (req, res) {
+	var id = req.params.id;
+
+	Movie.findById(id, function(err, movie){
+		Comment
+		.find({movie:id})
+		.populate('from', 'name')
+		.exec(function(err, comments) {
+			res.render('detail', {
+				title: 'movie' + movie.title,
+				movie: movie,
+				comments: comments
+			});
+		});
+	});
+};
 
 //admin page
 exports.new = function (req, res) {
