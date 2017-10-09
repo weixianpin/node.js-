4e76ddef3a26b66c468eaf4716cf27187c373e52
wayne <1372887803@qq.com>
2017-10-09 10:18:08 +0800

    隐藏域

---
+++ node&mongodb/public/js/detial.js
@@ -0,0 +1,18 @@
+$(function() {
+	$('.del').click(function(e) {
+		var target = $(this);
+		var toId = target.data('tid');
+		var commentId = target.data('cid');
+
+		$('<input>').attr({
+			type: 'hidden',
+			name: 'comment[tid]',
+			value: toId
+		}).appendTo('#commentForm');
+		$('<input>').attr({
+			type: 'hidden',
+			name: 'comment[cid]',
+			value: toId
+		}).appendTo('#commentForm');
+	});
+});
