d182fc785dd843703dd1c88587c84723617abdf8
wayne <1372887803@qq.com>
2017-10-09 15:42:56 +0800

    6

--- node&mongodb/app/controllers/movie.js
+++ node&mongodb/app/controllers/movie.js
@@ -37,9 +37,12 @@ exports.update = function(req, res) {
 	var id = req.params.id;
 	if(id) {
 		Movie.findById(id, function(err, movie) {
-			res.render('admin', {
-				title: 'movie 后台更新页面',
-				movie: movie
+			Category.find({}, function(err, categories) {
+				res.render('admin', {
+					title: 'movie 后台更新页面',
+					movie: movie,
+					categories: categories
+				});
 			});
 		});
 	}
@@ -66,17 +69,34 @@ exports.save = function(req, res) {
 		});
 	}else {
 		_movie = new Movie(movieObj);
-		var categoryId = _movie.category;
+		var categoryId = movieObj.category;
+		var categoryName = movieObj.categoryName;
 		_movie.save(function(err, movie) {
 				if(err) {
 					console.log(err);
 				}
-				Category.findById(categoryId, function(err, category) {
-					category.movies.push(movie._id);
-					category.save(function(err, category) {
-						res.redirect('/movie/' + movie._id);
+
+				if(categoryId) {
+					Category.findById(categoryId, function(err, category) {
+							category.movies.push(movie._id);
+							category.save(function(err, category) {
+								res.redirect('/movie/' + movie._id);
+							});
 					});
-				});
+				}
+				else if(categoryName) {
+					var category = new Category({
+						name: categoryName,
+						movies: [movie._id]
+					});
+					category.save(function(err, category) {
+						movie.category = category._id;
+						movie.save(function(err, category) {
+							res.redirect('/movie/' + movie._id);
+						});
+						
+					});
+				}
 		});
 	}
 };
--- node&mongodb/public/js/admin.js
+++ node&mongodb/public/js/admin.js
@@ -17,4 +17,27 @@ $(function() {
 			}
 		});
 	});
+
+	$("#douban").blur(function() {
+		var douban = $(this);
+		var id = douban.val();
+		if(id) {
+			$.ajax({
+				url: 'https://api.douban.com/v2/movie/object/' + id,
+				cache: true,
+				type: 'get',
+				dataType: 'jsonp',
+				crossDomin: true,
+				jsonp: 'callback',
+				success: function(data) {
+					$("#inputTitle").val(data.title);
+					$("#inputDirector").val(data.directors[0].name);
+					$("#inputCountry").val(data.coutries[0]);
+					$("#inputPoster").val(data.iamges.large);
+					$("#inputYear").val(data.year);
+					$("#inputSummary").val(data.summary);
+				}
+			});
+		}
+	});
 });
