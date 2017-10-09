810cb1a8ab3a8548eac68541ab45c1a5045f3fe1
wayne <1372887803@qq.com>
2017-10-09 17:36:14 +0800

    results.jade

--- node&mongodb/app/controllers/index.js
+++ node&mongodb/app/controllers/index.js
@@ -4,7 +4,11 @@ var Category =  require('../modules/category.js');
 exports.index = function (req, res) {
 	Category
 		.find({})
-		.populate({path: 'movies', options: {limit: 10}})
+		.populate({
+			path: 'movies', 
+			selector: 'title poster',
+			options: {limit: 10}
+		})
 		// 执行回调
 		.exec(function(err, categories) {
 			if (err) {
@@ -13,6 +17,37 @@ exports.index = function (req, res) {
 			res.render('index', {
 				title: 'movie 首页',
 				categories: categories
+			});
+		});
+};
+//search page
+exports.search = function (req, res) {
+	var catId = req.query.cat;
+	var page = req.query.p;
+	var index = page * 2;
+
+
+	Category
+		.find({})
+		.populate({
+			path: 'movies', 
+			selector: 'title poster',
+		})
+		// 执行回调
+		.exec(function(err, categories) {
+			if (err) {
+				console.log(err);
+			}
+			var category = categories[0] || {};
+			var movies = category.movies || [];
+			var results = movies.slice(index, index + 2);
+
+			res.render('results', {
+				title: 'movie 结果列表',
+				keyword: category.name,
+				currentPage: (page + 1),
+				totalPage: Math.ceil(movies.length / 2),
+				movies: category
 			});
 		});
 };
