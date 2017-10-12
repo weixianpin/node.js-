bbe4e028e9fbbaad067add331ec4629b6288cca3
wayne <1372887803@qq.com>
2017-10-12 14:03:53 +0800

    movie

--- node&mongodb/test/user/user.js
+++ node&mongodb/test/user/user.js
@@ -25,29 +25,29 @@ describe('<Unit Test', function() {
 			};
 			done();
 		});
-// 确定name用户不存在
-		describe('Before Method save:', function() {
-			it('should begin withour test user', function(done) {
-				User.find({name: user.name}, function(err, users) {
-					users.should.have.length(0);
+	// 确定name用户不存在
+	describe('Before Method save:', function() {
+		it('should begin withour test user', function(done) {
+			User.find({name: user.name}, function(err, users) {
+				users.should.have.length(0);
 
-					done();
+				done();
+			});
+		});
+	});
+	// 用户保存
+	describe('User save:', function() {
+		it('save without problems', function(done) {
+			var _user = new User(user);
+			_user.save(function(err) {
+				should.not.exist(err);
+				_user.remove(function(err) {
+					should.not.exist(err);
 				});
 			});
 		});
-// 用户保存
-		describe('User save:', function() {
-			it('save without problems', function(done) {
-				var _user = new User(user);
-				_user.save(function(err) {
-					should.not.exist(err);
-					_user.remove(function(err) {
-						should.not.exist(err);
-					});
-				});
-			});
 
-// 密码加密没有问题
+		// 密码加密没有问题
 		it('should password be hashed correctly', function(done) {
 			var password = user.password;
 			var _user = new User(user);
@@ -81,24 +81,27 @@ describe('<Unit Test', function() {
 				});
 			});
 		});
-// 是否存在重复值
+		// 是否存在重复值
 		it('should fail to save an existing user', function(done) {
 			var _user1 = new User(user);
-			_user1.save();
-
-			var _user2 = new User(user);
-			_user2.save(function(err) {
-				should.have(err);
-					
-				_user1.remove(function(err) {
-					if(!err) {
-						_user2.remove(function(err) {
-							done();
-						});
-					}
-					done();
+			_user1.save(function(err) {
+				var _user2 = new User(user);
+				_user2.save(function(err) {
+					should.have(err);
+						
+					_user1.remove(function(err) {
+						if(!err) {
+							_user2.remove(function(err) {
+								done();
+							});
+						}
+					});
 				});
 			});
 		});
+	});
+	after(function(done) {
+		// clear user fix
+		done();
 	});
 });
