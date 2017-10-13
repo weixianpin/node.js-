42071a63a9bccfe18aae47a9b0334d6e809139ae
wayne <1372887803@qq.com>
2017-10-13 17:39:28 +0800

    1

--- movie/public/js/admin.js
+++ movie/public/js/admin.js
@@ -21,7 +21,19 @@ $(function() {
     var douban = $(this)
     var id = douban.val()
 
-    iutPoster').val(data.images.large)
+    if (id) {
+      $.ajax({
+        url: 'https://api.douban.com/v2/movie/subject/' + id,
+        cache: true,
+        type: 'get',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'callback',
+        success: function(data) {
+          $('#inputTitle').val(data.title)
+          $('#inputDoctor').val(data.directors[0].name)
+          $('#inputCountry').val(data.countries[0])
+          $('#inputPoster').val(data.images.large)
           $('#inputYear').val(data.year)
           $('#inputSummary').val(data.summary)
         }
