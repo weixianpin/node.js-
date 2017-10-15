ed96fb80a8a790f74b68f3b4184827eaea774d6b
wayne <1372887803@qq.com>
2017-10-15 22:07:33 +0800

    2

--- movie/public/js/detail.js
+++ movie/public/js/detail.js
@@ -7,4 +7,25 @@ $(function() {
     if ($('#toId').length > 0) {
       $('#toId').val(toId)
     }
-    
+    else {
+      $('<input>').attr({
+        type: 'hidden',
+        id: 'toId',
+        name: 'comment[tid]',
+        value: toId
+      }).appendTo('#commentForm')
+    }
+
+    if ($('#commentId').length > 0) {
+      $('#commentId').val(commentId)
+    }
+    else {
+      $('<input>').attr({
+        type: 'hidden',
+        id: 'commentId',
+        name: 'comment[cid]',
+        value: commentId
+      }).appendTo('#commentForm')
+    }
+  })
+})
