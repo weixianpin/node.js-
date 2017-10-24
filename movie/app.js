955a32602370f164f598d9bb17300747e8079bf7
wayne <1372887803@qq.com>
2017-10-24 15:55:21 +0800

    app.js update

--- movie/app.js
+++ movie/app.js
@@ -9,10 +9,7 @@ var mongoStore = require('connect-mongo')(session)
 var logger = require('morgan')
 var serveStatic = require('serve-static')
 
-var port = process.env.PORT || 3000
-var app = express()
-var fs = require('fs')
-var dbUrl = 'mongodb://localhost/imooc'
+
 
 mongoose.connect(dbUrl)
 
